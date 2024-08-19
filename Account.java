641616183b26744667de57abea96126b0dca20b1
hiephp2606 <hoanghiep.hp2606@gmail.com>
2024-08-20 01:17:36 +0700

    lan cuoi that su

---
+++ Account.java
@@ -0,0 +1,10 @@
+public class Account {
+    public String username;
+    public String password;
+    public int balance;
+    public Account (String username, String password, int balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+}
--- main.java
+++ main.java
@@ -2,53 +2,65 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
-//        default input
-        String username = "techmaster";
-        String password = "hoclacoviec";
-        int balance = 1000000;
         Scanner sc = new Scanner(System.in);
+        Account acc = new Account("techmaster", "hoclacoviec", 1000000);
+
+        firstLap(acc,sc);
+    }
+
+//    vong lap 1
+    public static void firstLap (Account acc, Scanner sc) {
         boolean turn = true;
-
-
-
-//        system
         while (turn == true) {
-            //        insert input
             System.out.printf("Hãy nhập username: ");
             String ipuser = sc.nextLine();
             System.out.printf("Hãy nhập password: ");
             String ippassword = sc.nextLine();
-            if (ipuser.equals(username) && ippassword.equals(password)) {
-                system(sc, ipuser, ippassword, username, password, balance );
+            if ( ipuser.equals(acc.username) && ippassword.equals(acc.password)){
+                secondLap(sc, ipuser, ippassword, acc);
                 turn = false;
             }
-            else {
+            else{
                 System.out.println("Username hoặc Password sai, yêu cầu đăng nhập lại!");
             }
         }
     }
 
-
-//      system
-    public static boolean system(Scanner sc, String ipuser, String ippassword, String username, String password, int balance) {
+//     vong lap 2
+    public static boolean secondLap(Scanner sc, String ipuser, String ippassword, Account acc) {
         boolean turn = true;
         while (turn == true) {
             System.out.println("-Mời bạn chọn: \n  1. Xem thông tin tài khoản\n  2. Rút tiền");
             int choice = Integer.parseInt(sc.nextLine());
             if (choice == 1) {
-                System.out.println("user name: " + username);
-                System.out.println("password: " + password);
+                System.out.println("user name: " + acc.username);
+                System.out.println("password: " + acc.password);
+                turn = false;
+                thirdlap(sc, ipuser, ippassword, acc);
                 }
             else if (choice == 2) {
-                System.out.println("Số dư trong tài khoản: " + balance);
+                System.out.println("Số dư trong tài khoản: " + acc.balance);
+                turn = false;
+                thirdlap(sc, ipuser, ippassword, acc);
             }
             else {
                     System.out.println("Không có lựa chọn này xin lựa chọn lại");
-                    turn = true;
             }
+
+        }
+    return turn;
+    }
+
+//    vong lap 3
+    public static void thirdlap (Scanner sc, String ipuser, String ippassword, Account acc) {
+        boolean turn = true;
+        while (turn == true) {
             System.out.println("Bạn có muốn tiếp tục không? (Y/N)");
             String answer = sc.nextLine();
-            if (answer.equals("Y")) {}
+            if (answer.equals("Y")) {
+                turn = false;
+                secondLap(sc, ipuser, ippassword, acc);
+            }
             else if (answer.equals("N")) {
                 turn = false;
             }
@@ -56,6 +68,5 @@ public class main {
                 System.out.println("Vui lòng chỉ chọn Y hoặc N");
             }
         }
-    return turn;
     }
 }
